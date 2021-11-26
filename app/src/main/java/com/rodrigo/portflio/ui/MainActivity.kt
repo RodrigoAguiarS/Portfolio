d6b21b2e56df01758781289c8f2a4006fe862bdc
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-11-26 10:53:15 -0300

    Initial commit

---
+++ app/src/main/java/com/rodrigo/portflio/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rodrigo.portflio.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rodrigo.portflio.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
